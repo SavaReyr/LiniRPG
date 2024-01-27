546da137cda1c01c1e5a6f456ee567b0da988c6c
x1000sa <x1000.sa@yandex.ru>
2024-01-27 16:02:12 +0300

    Working screen mode selector

--- esc_menu.gd
+++ esc_menu.gd
@@ -8,10 +8,8 @@ func _on_exit_pressed():
 	get_tree().quit()
 	
 
-
-
 func _on_settings_pressed():
-	pass # Replace with function body.
+	print("Here is an option menu")
 
 
 func _on_resume_pressed():
