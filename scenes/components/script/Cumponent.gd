8b8b90cdb26a6d35c5c6d060b01ba8e45523aad5
x1000sag <x1000.sa@yandex.ru>
2024-01-27 22:24:48 +0300

    27.01 v2
    Co-Authored-By: SavaReyr <102984757+SavaReyr@users.noreply.github.com>

--- scenes/components/script/Cumponent.gd
+++ scenes/components/script/Cumponent.gd
@@ -4,7 +4,7 @@ extends Node
 @onready var child=Menu.instantiate()
 
 func _input(event):
-	if !get_parent().has_node("Esc_Menu"):
+	if !get_parent().has_node("Esc_Menu") and !get_parent().has_node("Window_button"):
 		if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
 			Menu = preload("res://scenes/ui/esc_menu.tscn")
 			child=Menu.instantiate()	
